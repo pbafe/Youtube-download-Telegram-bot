3eebfea361af438fdcbd210407883dc60180e57f
pbafe <pablobarranca@hotmail.com>
2023-12-06 15:17:56 +0100

    Update mega_helpers.py

--- mega_helpers.py
+++ mega_helpers.py
@@ -26,15 +26,15 @@ def upload(file_path):
         if (int(time.time()) - int(files[file]['ts'])) > 900: #900 sec == 15 min
             m.delete(file)
             print("Old, deleted: ", file)
-        #else:
-        #    print("New: ", file)
-
+    
+    # EMPTY BIN
+    m.empty_trash()
+    
     # Upload a file to the "downloads" folder
     file = m.upload(file_path, mega_folder)
     # Get the public link for the uploaded file
     link = m.get_upload_link(file)
 
-    # EMPTY BIN
-    m.empty_trash()
+    
     
     return link
